687388a534cd85d4307956d558b27d9aaf570993
dujiajun <jiajun_du@outlook.com>
2021-08-21 17:48:53 +0800

    fix cookie expires

--- oauth/views.py
+++ oauth/views.py
@@ -1,15 +1,19 @@
 import hashlib
+from datetime import timedelta
+
 from authlib.integrations.django_client import OAuth
 from authlib.jose import jwt
 from authlib.oidc.core import CodeIDToken
+from django.conf.global_settings import SESSION_COOKIE_AGE
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 # Create your views here.
 from django.urls import reverse
+from django.utils.datetime_safe import datetime
 
 from jcourse import settings
-from jcourse.settings import HASH_SALT
+from jcourse.settings import HASH_SALT, SESSION_COOKIE_SECURE, SESSION_COOKIE_HTTPONLY
 
 oauth = OAuth()
 oauth.register(
@@ -58,5 +62,8 @@ def auth_jaccount(request):
     hashed_username = hash_username(jaccount)
     login_with(request, hashed_username)
     response = redirect('/')
-    response.set_cookie('account', jaccount)
+    expires = datetime.strftime(datetime.utcnow() + timedelta(seconds=SESSION_COOKIE_AGE), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie('account', jaccount, expires=expires,
+                        samesite='lax',
+                        secure=SESSION_COOKIE_SECURE)
     return response
