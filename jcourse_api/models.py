68c33a35e91c424578cc7bc88c5653d8011d027d
dujiajun <jiajun_du@outlook.com>
2021-09-15 18:52:25 +0800

    add db_index to pinyin fields

--- jcourse_api/models.py
+++ jcourse_api/models.py
@@ -83,8 +83,8 @@ class Teacher(models.Model):
     name = models.CharField(verbose_name='姓名', max_length=255, null=False, blank=False, db_index=True)
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, verbose_name='单位', null=True, blank=True)
     title = models.CharField(verbose_name='职称', max_length=64, null=False, blank=True)
-    pinyin = models.CharField(verbose_name='拼音', max_length=64, null=True, blank=True)
-    abbr_pinyin = models.CharField(verbose_name='拼音缩写', max_length=64, null=True, blank=True)
+    pinyin = models.CharField(verbose_name='拼音', max_length=64, null=True, blank=True, db_index=True)
+    abbr_pinyin = models.CharField(verbose_name='拼音缩写', max_length=64, null=True, blank=True, db_index=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -118,8 +118,9 @@ class Review(models.Model):
         ordering = ['-created']
         constraints = [models.UniqueConstraint(fields=['user', 'course'], name='unique_review')]
 
-    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE, null=False, blank=False)
-    course = models.ForeignKey(Course, verbose_name='课程', on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE, null=False, blank=False, db_index=True)
+    course = models.ForeignKey(Course, verbose_name='课程', on_delete=models.CASCADE, null=False, blank=False,
+                               db_index=True)
     semester = models.ForeignKey(Semester, verbose_name='上课学期', on_delete=models.SET_NULL, null=True, blank=False)
     rating = models.IntegerField(verbose_name='推荐指数', validators=[MaxValueValidator(5), MinValueValidator(0)],
                                  null=False,
@@ -161,7 +162,7 @@ class Report(models.Model):
         ordering = ['-created']
         verbose_name_plural = verbose_name
 
-    user = models.ForeignKey(User, verbose_name='用户', null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='用户', null=False, blank=False, on_delete=models.CASCADE, db_index=True)
     solved = models.BooleanField(verbose_name='是否解决', default=False, null=False, blank=False)
     comment = models.TextField(verbose_name='反馈', max_length=817, null=False, blank=False)
     created = models.DateTimeField(verbose_name='发布时间', null=False, blank=False, default=timezone.now)
@@ -189,7 +190,8 @@ class Action(models.Model):
         constraints = [models.UniqueConstraint(fields=['user', 'review'], name='unique_action')]
 
     user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE, null=False, blank=False)
-    review = models.ForeignKey(Review, verbose_name='点评', on_delete=models.CASCADE, null=False, blank=False)
+    review = models.ForeignKey(Review, verbose_name='点评', on_delete=models.CASCADE, null=False, blank=False,
+                               db_index=True)
     action = models.IntegerField(choices=ACTION_CHOICES, verbose_name='操作', null=False, blank=False, default=0)
 
     def __str__(self):
@@ -216,7 +218,7 @@ class EnrollCourse(models.Model):
         verbose_name_plural = verbose_name
         constraints = [models.UniqueConstraint(fields=['user', 'course', 'semester'], name='unique_enroll')]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, db_index=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=False)
     semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, null=True, blank=True)
 
