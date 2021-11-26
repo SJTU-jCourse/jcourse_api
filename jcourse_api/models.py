f62bb8d646bc85224c0214508e319e29c5fd1c13
dujiajun <jiajun_du@outlook.com>
2021-11-26 12:08:39 +0800

    feat: update created when modify

--- jcourse_api/models.py
+++ jcourse_api/models.py
@@ -125,7 +125,7 @@ class Review(models.Model):
     semester = models.ForeignKey(Semester, verbose_name='上课学期', on_delete=models.SET_NULL, null=True)
     rating = models.IntegerField(verbose_name='推荐指数', validators=[MaxValueValidator(5), MinValueValidator(1)])
     comment = models.TextField(verbose_name='详细点评', max_length=817)
-    created = models.DateTimeField(verbose_name='发布时间', default=timezone.now)
+    created = models.DateTimeField(verbose_name='发布时间', auto_now=True)
     score = models.CharField(verbose_name='成绩', null=True, blank=True, max_length=10)
     moderator_remark = models.TextField(verbose_name='管理员批注', null=True, blank=True, max_length=817)
     approve_count = models.IntegerField(verbose_name='获赞数', null=True, blank=True, default=0)
@@ -204,7 +204,7 @@ class ApiKey(models.Model):
     key = models.CharField(max_length=255, unique=True)
     description = models.CharField(verbose_name='描述', max_length=255)
     is_enabled = models.BooleanField(verbose_name='启用', default=True)
-    last_modified = models.DateTimeField(verbose_name='修改时间', default=timezone.now)
+    last_modified = models.DateTimeField(verbose_name='修改时间', auto_now=True)
 
     def __str__(self):
         return f"{self.description}：{self.key} - {self.last_modified}"
