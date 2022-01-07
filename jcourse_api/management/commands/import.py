4952a69cf4efe3ff26d3ee989683ac0908bd7cf7
dujiajun <jiajun_du@outlook.com>
2022-01-07 22:38:51 +0800

    feat: add import command

---
+++ jcourse_api/management/commands/import.py
@@ -0,0 +1,38 @@
+from django.core.management import BaseCommand
+from tablib import Dataset
+
+from jcourse_api.admin import *
+
+
+class Command(BaseCommand):
+    help = 'Import from csv'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-f', '--file', type=str, help='filename')
+        parser.add_argument('-y', '--yes', action="store_true", help='dry run')
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-c', '--course', action="store_true", help='import courses')
+        group.add_argument('-t', '--teacher', action="store_true", help='import teachers')
+
+    def import_course(self, filename: str, dry_run: bool):
+        course_resource = CourseResource()
+        with open(filename, mode='r') as f:
+            dataset = Dataset().load(f)
+        result = course_resource.import_data(dataset, dry_run)
+        self.stdout.write(f'{result.has_errors()}')
+
+    def import_teacher(self, filename: str, dry_run: bool):
+        teacher_resource = TeacherResource()
+        with open(filename, mode='r') as f:
+            dataset = Dataset().load(f)
+        result = teacher_resource.import_data(dataset, dry_run)
+        self.stdout.write(f'{result.has_errors()}')
+
+    def handle(self, *args, **options):
+        if options['file']:
+            if options['course']:
+                self.import_course(options['file'], options['yes'])
+            elif options['teacher']:
+                self.import_teacher(options['file'], options['yes'])
+        else:
+            self.stdout.write(f'No filename provided!')
