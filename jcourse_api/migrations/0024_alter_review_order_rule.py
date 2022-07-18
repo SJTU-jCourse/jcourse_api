eac2ce20ec81ed4155ef1db34e6e1fea0b2c7e00
Zero2Light <894880052@qq.com>
2022-07-18 21:33:59 +0800

    feat:orderd by last-modified time

--- jcourse_api/migrations/0024_alter_review_order_rule.py
+++ jcourse_api/migrations/0024_alter_review_order_rule.py
@@ -3,13 +3,21 @@
 from django.db import migrations
 
 
-class Migration(migrations.Migration):
+def create_modified_time(apps, schema_editor):
+    Review = apps.get_model('jcourse_api', 'Review')
+    for review in Review.objects.filter(modified=None):
+        created_time = review.created
+        review.modified = created_time
+        review.save()
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('jcourse_api', '0023_notice_url'),
     ]
 
     operations = [
+        migrations.RunPython(create_modified_time),
         migrations.AlterModelOptions(
             name='review',
             options={'ordering': ['-modified'], 'verbose_name': '点评', 'verbose_name_plural': '点评'},
