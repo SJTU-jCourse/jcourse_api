e7e5b6b829fb0b434af8de34362abc40f829c088
Zero2Light <894880052@qq.com>
2022-08-27 23:47:58 +0800

    feat: email login

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,6 +4,7 @@ from oauth.views import *
 
 urlpatterns = [
     path('logout/', logout_auth, name='logout'),
+    path('login/', LoginWithEmailView.as_view(), name='login'),
     path('jaccount/login/', login_jaccount, name='login_jaccount'),
     path('jaccount/auth/', auth_jaccount, name='auth_jaccount'),
     path('sync-lessons/login/', sync_lessons_login, name='sync-lessons-login'),
--- oauth/views.py
+++ oauth/views.py
@@ -1,4 +1,6 @@
 import hashlib
+import re
+from random import Random
 
 from authlib.integrations.base_client import OAuthError
 from authlib.integrations.django_client import OAuth
@@ -6,8 +8,12 @@ from authlib.jose import jwt
 from authlib.oidc.core import CodeIDToken
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
+
+from django.core.cache import cache
+from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.urls import reverse
+from rest_framework.views import APIView
 
 from jcourse import settings
 from jcourse.settings import HASH_SALT
@@ -84,3 +90,58 @@ def sync_lessons_auth(request):
         return JsonResponse({'details': 'Bad argument!'}, status=400)
     request.session['token'] = token
     return JsonResponse({'details': 'Sync Status Ready!'})
+
+
+# 生成随机字符串
+def random_str(random_length=8):
+    rand_str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(random_length):
+        rand_str += chars[random.randint(0, length)]
+    return rand_str
+
+
+def send_code_email(email):
+    code = random_str(16)
+    email_title = "选课社区登录"
+    email_body = "您的登录验证码为：{0}，10分钟内有效，请尽快进行验证。".format(code)
+    send_status = send_mail(email_title, email_body, settings.DEFAULT_FROM_EMAIL, [email])
+    if not send_status:
+        return False
+    return code
+
+
+# 注册发送邮箱验证码
+class LoginWithEmailView(APIView):
+
+    def post(self, request):
+        email = request.POST.get("email", None)
+        code = request.POST.get("code", None)
+        # 发送验证码
+        if code is None:
+            sjtu_email = re.search('.*@sjtu.edu.cn$', email)
+            if sjtu_email is None:
+                response = JsonResponse({'status': 'error', 'message': '请输入交大邮箱！'}, status=400)
+                return response
+            else:
+                code = send_code_email(email)
+                if code:
+                    cache.set(email, code)
+                    response = JsonResponse({'status': 'success', 'message': '邮件已发送！'})
+                    return response
+                else:
+                    response = JsonResponse({'status': 'error', 'message': '验证码发送失败，请稍后重试。'}, status=400)
+                    return response
+        # 验证码验证
+        else:
+            if code == cache.get(email):
+                account = email.split('@')
+                hashed_username = hash_username(account[0])
+                login_with(request, hashed_username, 'email')
+                response = JsonResponse({'status': 'success', 'message': '用户' + account[0] + '登陆成功！'})
+                return response
+            else:
+                response = JsonResponse({'status': 'error', 'message': '验证码错误，请重试。'}, status=400)
+                return response
