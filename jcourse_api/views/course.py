3502862c608517ba6684a14e730ebca6b10d1d19
Du Jiajun <jiajun_du@outlook.com>
2022-12-15 10:35:11 +0800

    feat: order by id when count and avg equal

--- jcourse_api/views/course.py
+++ jcourse_api/views/course.py
@@ -51,8 +51,8 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
             courses = courses.filter(review_count__gt=0). \
                 annotate(count=F('review_count'), avg=F('review_avg'))
             if self.request.query_params['onlyhasreviews'] == 'count':
-                return courses.order_by(F('count').desc(nulls_last=True), F('avg').desc(nulls_last=True))
-            return courses.order_by(F('avg').desc(nulls_last=True), F('count').desc(nulls_last=True))
+                return courses.order_by(F('count').desc(nulls_last=True), F('avg').desc(nulls_last=True), "id")
+            return courses.order_by(F('avg').desc(nulls_last=True), F('count').desc(nulls_last=True), "id")
         return courses.all()
 
     def get_serializer_class(self):
