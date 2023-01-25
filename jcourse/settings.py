64e2a7359c28e318cac1f522ec72499bc5ef340a
Du Jiajun <jiajun_du@outlook.com>
2023-01-25 16:13:24 +0800

    dev: remove form in browsable api view

--- jcourse/settings.py
+++ jcourse/settings.py
@@ -175,7 +175,7 @@ REST_FRAMEWORK = {
 if DEBUG:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'jcourse.renderers.BrowsableAPIRendererWithoutForms',
     )
 
 CORS_ORIGIN_WHITELIST = [
