dc53df5f213d8d3bd06a3e4148064ae4ae950916
Du Jiajun <jiajun_du@outlook.com>
2023-02-16 11:36:26 +0800

    feat: use dotenv

--- jcourse/settings.py
+++ jcourse/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
