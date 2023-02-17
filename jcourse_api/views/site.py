5fa790ee9b4f1127278a4a3be34d1b86e9f31d67
Du Jiajun <jiajun_du@outlook.com>
2023-02-17 19:43:56 +0800

    feat: add more statistic info

--- jcourse_api/views/site.py
+++ jcourse_api/views/site.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
-from django.db.models import Count
-from django.db.models.functions import TruncDate
+from django.db.models import Count, F
+from django.db.models.functions import TruncDate, Floor
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import viewsets, status
@@ -22,14 +22,24 @@ class StatisticView(APIView):
 
     @method_decorator(cache_page(60))
     def get(self, request: Request):
-        user_join = User.objects.annotate(date=TruncDate("date_joined")).values("date").annotate(
+        user_join_time = User.objects.values(date=TruncDate("date_joined")).annotate(
             count=Count("id")).order_by("date")
-        review_create = Review.objects.annotate(date=TruncDate("created_at")).values("date").annotate(
+        review_create_time = Review.objects.values(date=TruncDate("created_at")).annotate(
             count=Count("id")).order_by("date")
-        return Response({'courses': Course.objects.count(),
-                         'courses_with_review': Course.objects.filter(review_count__gt=0).count(),
-                         'users': User.objects.count(),
-                         'reviews': Review.objects.count(),
-                         'user_join': user_join,
-                         'review_create': review_create},
+        course_review_count_dist = Course.objects.filter(review_count__gt=0).values(value=F("review_count")).annotate(
+            count=Count("value")).order_by("value")
+        course_review_avg_dist = Course.objects.filter(review_avg__gt=0).values(value=Floor("review_avg")).annotate(
+            count=Count("value")).order_by("value")
+        review_rating_dist = Review.objects.values(value=F("rating")).annotate(
+            count=Count("value")).order_by("value")
+        return Response({'course_count': Course.objects.count(),
+                         'course_with_review_count': Course.objects.filter(review_count__gt=0).count(),
+                         'user_count': User.objects.count(),
+                         'review_count': Review.objects.count(),
+                         'user_join_time': user_join_time,
+                         'review_create_time': review_create_time,
+                         'course_review_count_dist': course_review_count_dist,
+                         'course_review_avg_dist': course_review_avg_dist,
+                         'review_rating_dist': review_rating_dist
+                         },
                         status=status.HTTP_200_OK)
