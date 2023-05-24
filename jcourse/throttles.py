8a8b7044ea77dfe1953f95567d4a4acee0d492aa
Du Jiajun <jiajun_du@outlook.com>
2023-05-24 22:23:39 +0800

    feat: superuser exempt from throttle

--- jcourse/throttles.py
+++ jcourse/throttles.py
@@ -1,13 +1,22 @@
+from rest_framework.request import Request
 from rest_framework.throttling import UserRateThrottle
 
 
-class ReactionRateThrottle(UserRateThrottle):
+class SuperUserExemptRateThrottle(UserRateThrottle):
+
+    def allow_request(self, request: Request, view):
+        if request.user.is_superuser:
+            return True
+        return super().allow_request(request, view)
+
+
+class ReactionRateThrottle(SuperUserExemptRateThrottle):
     scope = 'review_reaction'
 
 
-class VerifyAuthRateThrottle(UserRateThrottle):
+class VerifyAuthRateThrottle(SuperUserExemptRateThrottle):
     scope = 'verify_auth'
 
 
-class EmailCodeRateThrottle(UserRateThrottle):
+class EmailCodeRateThrottle(SuperUserExemptRateThrottle):
     scope = 'email_code'
