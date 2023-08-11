edac5971f50eec398f7920cf21b367d06d1da907
Du Jiajun <jiajun_du@outlook.com>
2023-08-11 19:22:47 +0800

    feat: absolute url

--- ad/serializers.py
+++ ad/serializers.py
@@ -4,6 +4,11 @@ from ad.models import Promotion
 
 
 class PromotionSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Promotion
         fields = ('id', 'touchpoint', 'image', 'text', 'jump_link')
+
+    def get_image(self, obj: Promotion):
+        return obj.image.url
