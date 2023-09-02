f3f480925acd3eb6902b488cc495c0d3eb837b03
Du Jiajun <jiajun_du@outlook.com>
2023-09-02 16:01:09 +0800

    feat: can search by origin name

--- oauth/admin.py
+++ oauth/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from oauth.models import UserProfile
+from oauth.utils import hash_username
 
 
 @admin.register(UserProfile)
@@ -10,3 +11,9 @@ class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__username',)
     list_filter = ('user_type', 'lowercase', 'suspended_till')
     readonly_fields = ('user', 'user_type', 'lowercase')
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
+        hashed = hash_username(search_term)
+        queryset |= self.model.objects.filter(user__username=hashed)
+        return queryset, may_have_duplicates
